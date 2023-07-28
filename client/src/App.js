87000d5ea5382ce630d70093178becb2eee39be0
NecomaV <beken200225@gmail.com>
2023-07-29 00:32:36 +0600

    feat: login page
    still need to do redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,22 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-
+import Home from './pages/home';
+import Login from './pages/login';
 import PageRender from './PageRender';
 
 function App() {
   return (
     <Router>
-      <div className="flex justify-center align-center h-screen bg-violet-200">
+      <div className="h-screen">
 
       <input type="checkbox" id="theme"  className="peer/hide hidden"  />
       
       <div className="peer-checked/hide:invert w-full min-h-screen ">
-        <div className="max-w-5xl w-full m-auto">
+        <div className="">
         <Routes>
           <Route exact path="/:page/:id" element={<PageRender />} />
           <Route exact path="/:page" element={<PageRender />} />
+          <Route exact path="/" element={<Login/>} />
+
         </Routes>
         </div>
       </div>
--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,9 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 function Login() {
+
+  const initialState = {email: '', password: ''}
+  const [userData,setUserData] = useState(initialState)
+  const {email, password} = userData
+
+  const handleChangeInput = e => {
+    const {name, value} = e.target
+    setUserData({...userData, [name]:value})
+  }
+
   return (
-    <div>
-        Login
+    <div className='w-full h-screen flex justify-center items-center bg-indigo-50'>
+      <form className="bg-[url('./images/endless-constellation.svg')] w-full max-w-5xl bg-purple-50 rounded-xl shadow-2xl shadow-indigo-700 flex flex-col items-center p-20 h-4/6 drop-shadow-3xl">
+        <div className="mt-20">
+          <div className="md:flex md:items-center mb-6 w-full">
+            <div className="w-80">
+              <label className=" text-5xl text-gray-900 font-bold md:text-left mb-1 md:mb-0 pr-4" htmlFor="inline-full-name">
+                Username : 
+              </label>
+            </div>
+            <div className="md:w-1/2">
+              <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" 
+              type="text" 
+              name='email'
+              value={email}
+              onChange={handleChangeInput}/>
+            </div>
+          </div>
+          <div className="md:flex md:items-center mb-6 w-full" >
+            <div className="w-80">
+              <label className=" text-5xl  text-gray-900 font-bold md:text-left mb-1 md:mb-0 pr-4" htmlFor="inline-password">
+                Password :
+              </label>
+            </div>
+            <div className="md:w-1/2">
+              <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" 
+              id="inline-password"
+              type="password" 
+              placeholder="******************" 
+              value={password} 
+              onChange={handleChangeInput}
+              name='password'/>
+            </div>
+          </div>
+          <div className="md:flex md:items-center mb-6 w-full">
+            <div className="md:w-1"></div>
+            <label className="md:w-1/1 flex align-baseline text-gray-500 font-bold">
+              <input className="w-7 mr-4 rounded-b-2xl" type="checkbox"/>
+              <span className="text-2xl">
+                Send me your newsletter!
+              </span>
+            </label>
+          </div>
+          <div className="md:flex md:items-center w-full">
+            <div className="md:w-1/3"></div>
+            <div className="md:w-2/3">
+              <button className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white text-2xl font-bold py-2 px-4 rounded w-48 transition-all" type="button" disabled={email && password ? false : true}>
+                Login
+              </button>
+            </div>
+          </div>
+        </div>
+        <p className='mt-20 text-lg font-semibold'>
+          You dont have an account? 
+          </p>
+          <Link className='mt-2 text-lg font-semibold' to="/register"> Register Now</Link>
+      </form>
+      
     </div>
   )
 }
